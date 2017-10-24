318fd4a0ab2812d42c01f5cab1e895e367a4ae03
fisent <lukzieniewicz@gmail.com>
2017-10-25 00:43:44 +0200

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,14 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void loadJSON(String filename){
-
-
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static JSONArray getModels() throws FileNotFoundException {
         String jsonString = "";
         File f = new File("Blackout.json");
         Scanner scan  = new Scanner(f);
@@ -24,7 +21,30 @@ public class Main {
         }
 
 
+
         JSONObject object = new JSONObject(jsonString);
-        JSONArray array = object.getJSONArray("Packages");
+        JSONArray packages = object.getJSONArray("Packages");
+        JSONObject zeroObject = packages.getJSONObject(0);
+        return zeroObject.getJSONArray("Models");
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        JSONArray models = getModels();
+        ArrayList<JSONObject> resultList = new ArrayList<>();
+
+        JSONObject ob;
+        for(int i = 0; i<models.length();i++){
+            ob = models.getJSONObject(i);
+            if(ob.get("Type").equals("EventElement")){
+                resultList.add(ob);
+            }
+        }
+
+        for (JSONObject j : resultList){
+            System.out.println(j.getJSONObject("Properties").get("Text"));
+        }
+
     }
 }
